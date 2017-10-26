3dc3160cd88853a26da7fd541afa981817da88ad
lubrano <philippe@lubrano.fr>
2017-10-26 17:25:38 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,15 +3,18 @@
 date
 
 # traceroute
-traceroute -A files.pharo.org
-traceroute -A ci.inria.fr
+traceroute -A www.lri.fr
+#traceroute -A files.pharo.org
+#traceroute -A ci.inria.fr
 
 # latence serveurs
-httping -h files.pharo.org -c 10
-httping -h ci.inria.fr -c 10
+#httping -h files.pharo.org -c 10
+#httping -h ci.inria.fr -c 10
+httping -h www.lri.fr -c 10
 
 # Débit / chargement
-wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
-wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
+#wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
+#wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
+wget https://www.lri.fr/files/infoScientifique/aeres/LRIreport.pdf
 
 date
