dcea9999750f2d4cbdbbcd326d067c4a9c59aae2
lubrano <philippe@lubrano.fr>
2017-11-21 13:49:31 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ date
 # traceroute
 traceroute -A files.pharo.org
 traceroute -A ci.inria.fr
-traceroute -A vodcast.upmc.fr
+#traceroute -A vodcast.upmc.fr
 
 
 # latence serveurs
@@ -16,6 +16,6 @@ traceroute -A vodcast.upmc.fr
 # Débit / chargement
 wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
 wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
-wget http://vodcast.upmc.fr/telecharger.php?file=/2017/colloquium_lip6-2017/01-colloquium_lip6-interfaces_homme_machine-michel_beaudoin_lafon-28_02_2017.mp4
+#wget http://vodcast.upmc.fr/telecharger.php?file=/2017/colloquium_lip6-2017/01-colloquium_lip6-interfaces_homme_machine-michel_beaudoin_lafon-28_02_2017.mp4
 
 date
