eb461c30f9f93ed521d1f47bedb674f14311d412
lubrano <philippe@lubrano.fr>
2017-11-24 16:53:28 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 date
 
 # traceroute
-traceroute hal.archives-ouvertes.fr
+traceroute www-rocq.inria.fr
 #traceroute files.pharo.org
 #traceroute ci.inria.fr
 #traceroute -A vodcast.upmc.fr
@@ -15,7 +15,7 @@ traceroute hal.archives-ouvertes.fr
 #httping -h vodcast.upmc.fr -c 10
 
 # Débit / chargement
-wget https://hal.archives-ouvertes.fr/tel-01647220/document
+wget  https://www.rocq.inria.fr/gamma/gamma/Membres/CIPD/Loic.Marechal/Downloads/gm2.tgz
 wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
 #wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
 #wget http://vodcast.upmc.fr/telecharger.php?file=/2017/colloquium_lip6-2017/01-colloquium_lip6-interfaces_homme_machine-michel_beaudoin_lafon-28_02_2017.mp4
