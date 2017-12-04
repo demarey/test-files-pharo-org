cee5aa411ec07afed6740f217a2f23631b83d09e
demarey <christophe.demarey@inria.fr>
2017-12-04 10:36:37 +0100

    test pharo70 download

--- test.sh
+++ test.sh
@@ -16,9 +16,10 @@ traceroute -n -A files.pharo.org
 
 # Débit / chargement
 #wget  https://www.rocq.inria.fr/gamma/gamma/Membres/CIPD/Loic.Marechal/Downloads/gm2.tgz
-wget  http://files2.pharo.org/platform/Pharo6.0-linux.zip 
-wget  http://file-pharo.inria.fr/platform/Pharo6.0-linux.zip 
-wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
+wget http://files2.pharo.org/platform/Pharo6.0-linux.zip 
+wget http://file-pharo.inria.fr/platform/Pharo6.0-linux.zip 
+wget http://files.pharo.org/platform/Pharo6.0-linux.zip 
+wget http://files.pharo.org/get-files/70/pharo-linux-stable.zip
 #wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
 #wget http://vodcast.upmc.fr/telecharger.php?file=/2017/colloquium_lip6-2017/01-colloquium_lip6-interfaces_homme_machine-michel_beaudoin_lafon-28_02_2017.mp4
 
